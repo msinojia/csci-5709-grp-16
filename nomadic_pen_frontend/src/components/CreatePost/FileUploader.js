cc54070631ba7be7d468750498954f4074887dad
Meet Sinojia <mt845464@dal.ca>
2023-07-18 16:41:43 -0300

    Add backend

--- nomadic_pen_frontend/src/components/CreatePost/FileUploader.js
+++ nomadic_pen_frontend/src/components/CreatePost/FileUploader.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button } from "@mui/material";
 
-const FileUploader = ({ selectedFile, setSelectedFile, onFileChange }) => {
+const FileUploader = ({ onFileChange, selectedFile }) => {
   const [fileError, setFileError] = useState("");
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       "image/*": [],
     },
-    onDrop: (acceptedFiles) => {
+    disabled: !!selectedFile, // Disable dropzone when a file is already selected
+    onDropAccepted: (acceptedFiles) => {
       const file = acceptedFiles[0];
-      setSelectedFile(file);
-      setFileError("");
-      onFileChange(file);
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const base64String = reader.result;
+        setFileError("");
+        onFileChange(base64String);
+      };
+
+      reader.onerror = () => {
+        setFileError("Error occurred while reading the file.");
+      };
+
+      reader.readAsDataURL(file);
     },
     onDropRejected: () => {
+      onFileChange(null);
       setFileError("Invalid file type. Please select an image.");
     },
   });
@@ -27,28 +39,42 @@ const FileUploader = ({ selectedFile, setSelectedFile, onFileChange }) => {
         style={{ marginBottom: "1rem", textAlign: "center" }}
       >
         <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the image here...</p>
-        ) : (
-          <p>Drag and drop an image here or click to browse</p>
+        {!selectedFile && (
+          <>
+            {isDragActive ? (
+              <p>Drop the image here...</p>
+            ) : (
+              <p>Drag and drop an image here or click to browse</p>
+            )}
+            <Button variant="contained">Browse</Button>
+          </>
         )}
-        <Button variant="contained">Browse</Button>
-        {selectedFile && (
-          <p>
-            Selected File: {selectedFile.name} ({selectedFile.size} bytes)
-          </p>
-        )}
-        {fileError && <p style={{ color: "red" }}>{fileError}</p>}
       </div>
-      {selectedFile && selectedFile.type.startsWith("image/") && (
-        <div style={{ textAlign: "center" }}>
-          <img
-            src={URL.createObjectURL(selectedFile)}
-            alt="Selected"
-            style={{ maxWidth: "100%", maxHeight: "200px" }}
-          />
+      {selectedFile && selectedFile.startsWith("data:image/") && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ textAlign: "center" }}>
+            <img
+              src={selectedFile}
+              alt="Selected"
+              style={{ maxWidth: "100%", maxHeight: "200px" }}
+            />
+          </div>
+          <Button
+            variant="contained"
+            style={{ marginTop: "1rem" }}
+            onClick={() => onFileChange(null)}
+          >
+            Remove
+          </Button>
         </div>
       )}
+      {fileError && <p style={{ color: "red" }}>{fileError}</p>}
     </div>
   );
 };
