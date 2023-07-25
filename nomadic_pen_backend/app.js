ae652f773d386b4e3e02f41c952f3566120bfe27
Taha Mustufa Zanzibarwala <th803624@dal.ca>
2023-07-25 17:37:17 -0300

    Travel-Guide MVP Taha

--- nomadic_pen_backend/app.js
+++ nomadic_pen_backend/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connectDB = require("./db");
 const postRoutes = require("./routes/posts");
 const scheduledPostRoutes = require("./routes/scheduledPosts");
+const travelGuideRoutes = require("./routes/travelGuideRoutes")
 
 const app = express();
 app.use(express.json({ limit: "10mb" }));
@@ -16,6 +17,7 @@ connectDB();
 // Define API routes
 app.use("/posts", postRoutes);
 app.use("/scheduled-posts", scheduledPostRoutes);
+app.use("/nomadic-pen", travelGuideRoutes)
 
 // Start the server
 const port = 8000;
