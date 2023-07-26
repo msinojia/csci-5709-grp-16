37c95b777bf36c9bcd6ef8fd670671fc1dc0188a
Sreejith Nair <sr816885@dal.ca>
2023-07-26 03:17:52 -0300

    Adding article list backend changes

--- nomadic_pen_frontend/src/components/Profile/ArticleBox.js
+++ nomadic_pen_frontend/src/components/Profile/ArticleBox.js
@@ -1,35 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import { List, ListItem, Box, Typography, Card, CardContent, CardMedia, CardActionArea} from '@mui/material';
 const ArticleBox = () => {
+    const [articles, setArticles] = useState([]);
     const handleArticleClick = (articleId) => {
         alert('Redirecting to the Article Page');
     };
 
-    const mockArticles = [
-        { id: 1, title: 'Clickable Article 1', image:'assets/profile-page/card-images/istockphoto-5.jpg'},
-        { id: 2, title: 'Clickable Article 2', image:'assets/profile-page/card-images/istockphoto-4.jpg'},
-        { id: 3, title: 'Clickable Article 3', image:'assets/profile-page/card-images/istockphoto-3.jpg'},
-        { id: 4, title: 'Clickable Article 4', image:'assets/profile-page/card-images/istockphoto-1.jpg' },
-        { id: 5, title: 'Clickable Article 5', image:'assets/profile-page/card-images/istockphoto-6.jpg' },
-        { id: 6, title: 'Clickable Article 6', image:'assets/profile-page/card-images/istockphoto-5.jpg' },
-        { id: 7, title: 'Clickable Article 7', image:'assets/profile-page/card-images/istockphoto-2.jpg' },
-        { id: 8, title: 'Clickable Article 8', image:'assets/profile-page/card-images/istockphoto-3.jpg' },
-        { id: 8, title: 'Clickable Article 9', image:'assets/profile-page/card-images/istockphoto-4.jpg' },
-        { id: 8, title: 'Clickable Article 10', image:'assets/profile-page/card-images/istockphoto-6.jpg' },
-    ];
+    useEffect(() => {
+        axios.get('http://localhost:8000/fetchAllArticles')
+            .then((response) => {
+                setArticles(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching articles:', error);
+            });
+    }, []);
     return (
         <>
             <h3>My Posted Articles</h3>
             <Box name="articleBox" sx={{ display: 'flex', maxHeight: '410px', overflow: 'auto', border: '1px solid black', borderRadius: '4px' }}>
                 <List sx={{ width: '100%'}}>
-                    {mockArticles.map((article) => (
-                        <ListItem key={article.id}>
+                    {articles.map((article) => (
+                        <ListItem key={article._id}>
                             <Card sx={{ width: '100%' }}>
                                 <CardActionArea onClick={() => handleArticleClick(article.id)}>
                                     <CardMedia
                                         component="img"
                                         height="140"
-                                        image= {article.image}
+                                        image= {article.featuredImage}
                                         alt={article.title}
                                     />
                                     <CardContent>
