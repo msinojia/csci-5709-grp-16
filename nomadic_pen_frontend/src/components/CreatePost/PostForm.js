de2b78fab51ee8c63642b2ad2f5248cecc403107
Meet Sinojia <mt845464@dal.ca>
2023-07-26 01:00:55 -0300

    Display dialog after scheduling a post

--- nomadic_pen_frontend/src/components/CreatePost/PostForm.js
+++ nomadic_pen_frontend/src/components/CreatePost/PostForm.js
@@ -10,6 +10,11 @@ import {
   Menu,
   MenuItem,
   Paper,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
 } from "@mui/material";
 import { ArrowDropDown as ArrowDropDownIcon } from "@mui/icons-material";
 import axios from "axios";
@@ -30,6 +35,7 @@ const PostForm = () => {
   const [tags, setTags] = useState([]);
   const [tagInput, setTagInput] = useState("");
   const [anchorEl, setAnchorEl] = useState(null);
+  const [scheduleConfirmOpen, setScheduleConfirmOpen] = useState(false);
 
   const [errors, setErrors] = useState({
     title: false,
@@ -94,6 +100,11 @@ const PostForm = () => {
   const handleSchedule = async (selectedDate) => {
     setIsScheduleOpen(false);
     await createPost(selectedDate);
+  };
+
+  const handleScheduleConfirmClose = async () => {
+    setScheduleConfirmOpen(false);
+    navigate("/posts/following");
   };
 
   const handleSubmit = async (event) => {
@@ -165,11 +176,11 @@ const PostForm = () => {
           setEditorHtmlValue("");
           setTags([]);
           setTagInput("");
-          
-          if (scheduledDateTime) {
+
+          if (!scheduledDateTime) {
             navigate("/posts/" + response.data.data._id);
           } else {
-            navigate("/posts/following");
+            setScheduleConfirmOpen(true);
           }
         } else {
           console.log("Error creating post:", response);
@@ -309,6 +320,28 @@ const PostForm = () => {
             onClose={handleScheduleClose}
             onSchedule={handleSchedule}
           />
+          <Dialog
+            open={scheduleConfirmOpen}
+            onClose={handleScheduleConfirmClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">Post Scheduled</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Your blog post has been scheduled successfully!
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={handleScheduleConfirmClose}
+                color="primary"
+                autoFocus
+              >
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
         </form>
       </Paper>
     </Box>
